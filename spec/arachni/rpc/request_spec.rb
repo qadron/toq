dbd9302c21e9c0cba0900f9a8ce5ac7e21f42374
Tasos Laskos <tasos.laskos@gmail.com>
2014-05-01 17:12:31 +0300

    Request: Code cleanup

--- spec/arachni/rpc/request_spec.rb
+++ spec/arachni/rpc/request_spec.rb
@@ -1,54 +1,52 @@
-require File.join( File.expand_path( File.dirname( __FILE__ ) ), '../../', 'spec_helper' )
+require 'spec_helper'
 
 describe Arachni::RPC::Request do
-
-    before( :all ) do
-        @r = Arachni::RPC::Request
-    end
+    subject { described_class.new }
 
     describe '#message' do
         it 'should be an accessor' do
-            r = @r.new
-            r.message = 'test'
-            r.message.should == 'test'
+            subject.message = 'test'
+            subject.message.should == 'test'
         end
     end
 
     describe '#args' do
         it 'should be an accessor' do
-            r = @r.new
-            r.args = %w(test)
-            r.args.should == %w(test)
+            subject.args = %w(test)
+            subject.args.should == %w(test)
         end
     end
 
     describe '#token' do
         it 'should be an accessor' do
-            r = @r.new
-            r.token = 'blah'
-            r.token.should == 'blah'
+            subject.token = 'blah'
+            subject.token.should == 'blah'
         end
     end
 
     describe '#callback' do
         it 'should be an accessor' do
-            r = @r.new
             called = false
-            r.callback = proc { called = true }
-            r.callback.call
+            subject.callback = proc { called = true }
+            subject.callback.call
             called.should be_true
         end
     end
 
     describe '#prepare_for_tx' do
         it 'should convert the request to a hash ready for transmission' do
-            r = @r.new
-            r.prepare_for_tx.should be_empty
+            subject.prepare_for_tx.should be_empty
 
-            r = @r.new( message: 'obj.method', args: %w(test), token: 'mytoken',
+            described_class.new(
+                message:  'obj.method',
+                args:     %w(test),
+                token:    'mytoken',
                 callback: proc{}
-            )
-            r.prepare_for_tx.should be_eql( "args" => %w(test), "message" => "obj.method", "token" => "mytoken" )
+            ).prepare_for_tx.should =={
+                'args'    => %w(test),
+                'message' => 'obj.method',
+                'token'   => 'mytoken'
+            }
         end
     end
 
