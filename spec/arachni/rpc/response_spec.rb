6a959ced8784ec38571b0bc6ec8ed89bae8bdab9
Tasos Laskos <tasos.laskos@gmail.com>
2014-05-01 17:14:43 +0300

    Response: Code cleanup

--- spec/arachni/rpc/response_spec.rb
+++ spec/arachni/rpc/response_spec.rb
@@ -1,29 +1,26 @@
-describe Arachni::RPC::Response do
+require 'spec_helper'
 
-    before( :all ) do
-        @r = Arachni::RPC::Response
-    end
+describe Arachni::RPC::Response do
+    subject { described_class.new }
 
     describe '#obj' do
         it 'should be an accessor' do
-            r = @r.new
-            r.obj = 'test'
-            r.obj.should == 'test'
+            subject.obj = 'test'
+            subject.obj.should == 'test'
         end
     end
 
     describe '#async?' do
         context 'by default' do
             it 'should return false' do
-                @r.new.async?.should be_false
+                subject.async?.should be_false
             end
         end
 
         context 'after #async!' do
             it 'should return false' do
-                r = @r.new
-                r.async!
-                r.async?.should be_true
+                subject.async!
+                subject.async?.should be_true
             end
         end
     end
