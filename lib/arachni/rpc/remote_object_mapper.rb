9e9cdb3cd8feff114a2ce0867cc844afad0eccb0
Tasos Laskos <tasos.laskos@gmail.com>
2014-05-01 17:20:14 +0300

    RemoteObjectMapper: Code cleanup

--- lib/arachni/rpc/remote_object_mapper.rb
+++ lib/arachni/rpc/remote_object_mapper.rb
@@ -6,57 +6,50 @@
 
 =end
 
-require File.join( File.expand_path( File.dirname( __FILE__ ) ), '../', 'rpc' )
-
 module Arachni
 module RPC
 
-#
 # Maps the methods of remote objects to local ones.
-# (Well, not really, it just passes the message along to the remote end.)
 #
 # You start like:
 #
-#    server = Arachni::RPC::EM::Client.new( host: 'localhost', port: 7331 )
-#    bench  = Arachni::RPC::EM::Client::Mapper.new( server, 'bench' )
+#     client = Arachni::RPC::Client.new( host: 'localhost', port: 7331 )
+#     bench  = Arachni::RPC::RemoteObjectMapper.new( client, 'bench' )
 #
 # And it allows you to do this:
 #
-#    res = bench.foo( 1, 2, 3 )
+#     res = bench.foo( 1, 2, 3 )
 #
 # Instead of:
 #
-#    res = client.call( 'bench.foo', 1, 2, 3 )
-#
-#
+#     res = client.call( 'bench.foo', 1, 2, 3 )
 #
 # The server on the other end must have an appropriate handler set, like:
 #
-#    class Bench
-#        def foo( i = 0 )
-#            return i
-#        end
-#    end
+#     class Bench
+#         def foo( i = 0 )
+#             return i
+#         end
+#     end
 #
-#    server = Arachni::RPC::EM::Server.new( host: 'localhost', port: 7331 )
-#    server.add_handler( 'bench', Bench.new )
+#     server = Arachni::RPC::Server.new( host: 'localhost', port: 7331 )
+#     server.add_handler( 'bench', Bench.new )
 #
 # @author Tasos "Zapotek" Laskos <tasos.laskos@gmail.com>
-#
 class RemoteObjectMapper
 
-    def initialize( server, remote )
-        @server = server
-        @remote = remote
+    # @param    [Client]    client
+    # @param    [String]    handler
+    def initialize( client, handler )
+        @client = client
+        @handler = handler
     end
 
     private
-    #
+
     # Used to provide the illusion of locality for remote methods
-    #
     def method_missing( sym, *args, &block )
-        call = "#{@remote}.#{sym.to_s}"
-        @server.call( call, *args, &block )
+        @client.call( "#{@handler}.#{sym.to_s}", *args, &block )
     end
 
 end
